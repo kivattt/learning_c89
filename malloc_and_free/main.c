e0075e18570c822513fe8fa98b1fe080630db202
kivattt <kivattt@gmail.com>
2025-08-19 16:35:15 +0200

    Update

--- malloc_and_free/main.c
+++ malloc_and_free/main.c
@@ -1,3 +1,4 @@
+#include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,11 +6,21 @@ typedef unsigned char byte;
 
 int main() {
 	byte *data = (byte*)malloc(256);
-	int i = 0;
+	int i;
+	int number = 0;
 
-	for (; i < 256; i++) {
+	for (i = 0; i < 256; i++) {
 		data[i] = (byte)i;
 	}
+
+	for (i = 0; i < 256 / 4; i++) {
+		number |= *((int*)data+i);
+	}
+
+	/*number = *((int*)data+0);*/
+	/*number = _bswap(*((int*)data));*/
+
+	printf("%d\n", number);
 
 	free(data);
 	return 0;
