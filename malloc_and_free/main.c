8e54add3d56aad5e9a7edcdfca6434ee8dc881da
kivattt <kivattt@gmail.com>
2025-08-19 17:56:21 +0200

    Update

--- malloc_and_free/main.c
+++ malloc_and_free/main.c
@@ -1,8 +1,28 @@
 #include <immintrin.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef unsigned char byte;
+
+char alpha[] = "0123456789abcdef";
+
+void print_hex(int number) {
+	int i;
+	ssize_t shouldBeOne;
+	for (i = 0; i < 8; i++) {
+		char *c = &alpha[(number >> (32 - 4*i)) & 0xF];
+		shouldBeOne = write(1, c, 1);
+		if (shouldBeOne != 1) {
+			printf("epic fail\n");
+		}
+	}
+
+	shouldBeOne = write(1, "\n", 1);
+	if (shouldBeOne != 1) {
+		printf("epic fail\n");
+	}
+}
 
 int main() {
 	byte *data = (byte*)malloc(256);
@@ -13,12 +33,12 @@ int main() {
 		data[i] = (byte)i;
 	}
 
+	number = *((int*)data);
+	print_hex(number);
+
 	for (i = 0; i < 256 / 4; i++) {
 		number |= *((int*)data+i);
 	}
-
-	/*number = *((int*)data+0);*/
-	/*number = _bswap(*((int*)data));*/
 
 	printf("%d\n", number);
 
